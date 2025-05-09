ef2ff7b42c34303384f063b1613898b574ec4e22
EmnaSahnoun <137176942+EmnaSahnoun@users.noreply.github.com>
2025-05-09 12:47:38 +0100

    Update RabbitMQConfig.java

--- Back/MicroserviceEureka/ProjectService/src/main/java/com/example/ProjectService/config/RabbitMQConfig.java
+++ Back/MicroserviceEureka/ProjectService/src/main/java/com/example/ProjectService/config/RabbitMQConfig.java
@@ -80,7 +80,7 @@ public class RabbitMQConfig {
         factory.setAcknowledgeMode(AcknowledgeMode.AUTO); // Gestion automatique des accusés de réception
         return factory;
     }
-    @Bean
+    /*@Bean
     public Queue dlq() {
         return QueueBuilder.durable("queue.ActivityService.taskCreated.DLQ").build();
     }
@@ -90,5 +90,5 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(dlq())
                 .to(exchange())
                 .with("ProjectService.envoye.ActivityService.DLQ");
-    }
+    }*/
 }
