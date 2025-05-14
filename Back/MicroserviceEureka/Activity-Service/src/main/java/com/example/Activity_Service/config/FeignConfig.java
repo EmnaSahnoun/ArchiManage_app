d28d25c2091beaa0a466ff40d9b7db42d7b2fb12
EmnaSahnoun <137176942+EmnaSahnoun@users.noreply.github.com>
2025-05-14 02:54:33 +0100

    Update FeignConfig.java

--- Back/MicroserviceEureka/Activity-Service/src/main/java/com/example/Activity_Service/config/FeignConfig.java
+++ Back/MicroserviceEureka/Activity-Service/src/main/java/com/example/Activity_Service/config/FeignConfig.java
@@ -23,7 +23,6 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 
 @Configuration
 public class FeignConfig {
-    
     @Bean
     public Decoder feignDecoder() {
         ObjectFactory<HttpMessageConverters> messageConverters = () -> new HttpMessageConverters(
