78b7623d91f3129bbb7f571076d36f853a5929d5
EmnaSahnoun <137176942+EmnaSahnoun@users.noreply.github.com>
2025-05-15 00:12:59 +0100

    Update RabbitMQConfig.java

--- Back/MicroserviceEureka/NotificationService/src/main/java/com/example/NotificationService/config/RabbitMQConfig.java
+++ Back/MicroserviceEureka/NotificationService/src/main/java/com/example/NotificationService/config/RabbitMQConfig.java
@@ -9,9 +9,10 @@ import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.nio.charset.StandardCharsets;
-
+@Configuration
 public class RabbitMQConfig {
     @Value("${rabbitmq.queueJson2.name}")
     private String queueName;
