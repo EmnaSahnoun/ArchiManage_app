a8a51906b744c37043dddd9db8bc71185f38fb63
EmnaSahnoun <137176942+EmnaSahnoun@users.noreply.github.com>
2025-05-14 15:22:24 +0100

    Update NotificationController.java

--- Back/MicroserviceEureka/NotificationService/src/main/java/com/example/NotificationService/controller/NotificationController.java
+++ Back/MicroserviceEureka/NotificationService/src/main/java/com/example/NotificationService/controller/NotificationController.java
@@ -1,7 +1,11 @@
 package com.example.NotificationService.controller;
 
+import com.example.NotificationService.consumer.CommentNotificationConsumer;
 import com.example.NotificationService.services.SSENotificationService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +17,11 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequiredArgsConstructor
 public class NotificationController {
     private final SSENotificationService sseNotificationService;
+    private static final Logger logger = LoggerFactory.getLogger(NotificationController.class);
 
-    @GetMapping("/subscribe/{userId}")
+    @GetMapping(value = "/subscribe/{userId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@PathVariable String userId) {
+        logger.info("New SSE subscription for user: {}", userId);
         return sseNotificationService.subscribe(userId);
     }
 }
